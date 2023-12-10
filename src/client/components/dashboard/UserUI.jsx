37310542df33074558495c2a517793bcbce7e4f6
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-12-10 22:08:04 +0100

    changes

--- src/client/components/dashboard/UserUI.jsx
+++ src/client/components/dashboard/UserUI.jsx
@@ -4,7 +4,7 @@ function UserUI() {
   return (
     <div className='w-full bg-primary/5 flex justify-end items-center gap-5 p-5'>
       <p>
-        Hi <span className='capitalize'>Mahdi !</span>
+        Hi <span className='capitalize'>Mahdi Idrissi !</span>
       </p>
       <IoSettings className='cursor-pointer text-[25px] text-primary transition-all duration-200 hover:text-primary/30 ' />
       <LogOut />
